2837947d961221ac162ef15a5bae038827ede521
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-03-14 14:18:46 +0000

    impl basic value reading code for FIBS
    git-svn-id: http://svn.tonic-water.com/projects/public/bgutil/trunk@1005 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- board/FIBS.py
+++ board/FIBS.py
@@ -5,47 +5,61 @@
 # Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
 #
 
-def FIBSDecode(s):
-  class FIBSBoardState:
-    index = dict(
-        you=(1, 2), 
-        him=(2, 3),
-        matchlength=(3, 4),
-        your_score=(4, 5),
-        his_score=(5, 6),
-        board=(6, 32),
-        turn=(32, 33),
-        dice=(),
-        may_double=(),
-        was_doubled=(),
-        colour=(),#ugh!
-        direction=(),#ugh!
-        OnHome=(), 
-        OnBar=(),
-        CanMove=(),
-        ForcedMove=(),# Not USED! 
-        Redoubles=(),
-      )
+class _FIBSBoardState(object):
+  '''
+  http://www.fibs.com/fibs_interface.html#board_state
+  '''
+  index = dict(
+      you=(1, 2), 
+      him=(2, 3),
+      matchlength=(3, 4),
+      your_score=(4, 5),
+      his_score=(5, 6),
+      board=(6, 32),
+      turn=(32, 33),
+      your_dice=(33, 35),
+      his_dice=(35, 37),
+      doubling_cube=(37, 38),
+      you_may_double=(38, 39),
+      he_may_double=(39, 40),
+      was_doubled=(40, 41), # maybe not used ?
+      your_colour=(41, 42),
+      your_direction=(42, 43),
+      your_home=(43, 44), #obsolete
+      your_bar=(44, 45), 
+      your_chequers_on_home=(45, 46),
+      his_chequers_on_home=(46, 47),
+      your_chequers_on_bar=(47, 48),
+      his_chequers_on_bar=(48, 49),
+      your_chequers_to_play=(49, 50),
+      forced_move_and_did_crawford=(50, 52),# Not USED! 
+      redoubles=(52, 53),
+    )
 
-    def __init__(self, s):
-      ''' see bellow address for definition.
-      http://www.fibs.com/fibs_interface.html#board_state
-      '''
-      self.data = s.split(':')
+  def __init__(self, s):
+    self.data = s.split(':')
 
-  class Decoded(FIBSBoardState):
-    def __init__(self, s):
-      FIBSBoardState.__init__(self, s)
-      self.position = None
+  def __getattr__(self, name):
+    def cast_if_possible(x):
+      try:
+        return int(x)
+      except:
+        return x
+    L = map(cast_if_possible, self.data[self.index[name][0]:self.index[name][1]])
+    if len(L) == 1:
+      return L[0]
+    else:
+      return tuple(L)
 
-  ret = Decoded(s)
-  ret.position = \
-((0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0,\
+  def position(self):
+    return ((0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0,\
  5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0),\
  (0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0,\
  5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0))
-  return ret
 
+
+def FIBSDecode(s):
+  return _FIBSBoardState(s)
 
 def FIBSEncode(s):
   '''currently no use.'''
