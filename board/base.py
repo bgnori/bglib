ce4aaf56ac0f001c41f844579704d17825963f21
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-03-16 06:26:06 +0000

    fix move BitArray from gnubg.py to base.py
    git-svn-id: http://svn.tonic-water.com/projects/public/bgutil/trunk@1022 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- board/base.py
+++ board/base.py
@@ -219,6 +219,141 @@ def twoside_decode(s):
   return (tuple(xs[:25]), tuple(xs[25:50]))
 
 
+
+bits_in_byte = 8 # 1 byte == 8 bits
+
+def byte_length(length_in_bit):
+  if length_in_bit % bits_in_byte:
+    roundup = 1
+  else:
+    roundup = 0
+  return length_in_bit / bits_in_byte + roundup
+
+
+import struct
+
+class BitArray:
+  strcut_fmt = '!B'
+  def __init__(self, size, binary=None, endian=None):
+    self.size = size
+    if binary: 
+      if len(binary) > byte_length(size):
+        raise ValueError('spilling data, %i byte is too long for array size %i bits !'
+                         %(len(binary), size))
+      self.binary = binary
+    else:
+      self.binary = '\x00'*byte_length(self.size)
+    if endian:
+      self.endian = endian
+    else:
+      self.endian = '<' # little endian
+
+  def __len__(self):
+    return self.size
+
+  def int(self):
+    ret = 0
+    mask = 1
+    for bit in self:
+      if bit:
+        ret |= mask
+      mask = mask << 1
+    return ret
+
+
+  def _getbyte(self, pos_of_byte):
+    return struct.unpack(self.strcut_fmt, self.binary[pos_of_byte])[0]
+  
+  def _setbyte(self, pos_of_byte, value):
+    self.binary = (self.binary[:pos_of_byte]
+                   + struct.pack(self.strcut_fmt, value) 
+                   + self.binary[pos_of_byte+1:]
+                   )[:self.size]
+
+  def _pos_in_byte(self, nth):
+    assert(isinstance(nth, int))
+    assert(nth < bits_in_byte)
+    assert(0 <= nth)
+    if self.endian == '<':
+      return nth%bits_in_byte
+    elif self.endian == '>':
+      return 7 - nth%bits_in_byte
+    
+  def _getpos(self, nth):
+    assert(isinstance(nth, int))
+    if 0 >  nth or nth >= self.size:
+      raise IndexError('out of range')
+    return (nth/bits_in_byte, self._pos_in_byte(nth%bits_in_byte))
+
+  def getnth(self, nth):
+    pos_of_byte, pos_in_byte = self._getpos(nth)
+
+    byte = self._getbyte(pos_of_byte)
+    if byte & 1 << pos_in_byte:
+      return 1
+    else:
+      return 0
+
+  def __getitem__(self, nth_or_slice):
+    if isinstance(nth_or_slice, int):
+      return self.getnth(nth_or_slice)
+    elif isinstance(nth_or_slice, slice):
+      assert(nth_or_slice.step is None)# or nth_or_slice.step == 1)
+      slice_length = nth_or_slice.stop - nth_or_slice.start
+      ret = BitArray(size=slice_length, 
+                     endian=self.endian
+                      )
+      for i in range(ret.size):
+        ret[i] = self.getnth(nth_or_slice.start + i)
+      return ret
+    else:
+      raise TypeError('index must be int or slice, but got %s'%str(type(nth_or_slice)))
+
+  def __setitem__(self, nth, value):
+    assert(isinstance(nth, int))
+    if not value in (0, 1):
+      raise ValueError('value for asignment must be 0 or 1')
+    pos_of_byte, pos_in_byte = self._getpos(nth)
+    data = self._getbyte(pos_of_byte)
+
+    if value:
+      data |= 1 << pos_in_byte
+    else:
+      data &= ~(1 << pos_in_byte)
+
+    self._setbyte(pos_of_byte, data)
+
+  def __iter__(self):
+    for i in range(self.size):
+      yield self[i]
+
+  def __repr__(self):
+    return "<BitArray Instance '%s'>"%(':'.join(map(str, list(self))))
+
+  '''unsupported'''
+  def __contains__(self, item):raise NotImplemented
+  def __delitem__(self, key):raise NotImplemented
+  def __add__(self, x):raise NotImplemented
+  def __radd__(self, x):raise NotImplemented
+  def __iadd__(self, x):raise NotImplemented
+  def __mul__(self, x): raise NotImplemented
+  def __rmul__(self, x): raise NotImplemented
+  def __imul__(self, x): raise NotImplemented
+
+
+def single_int(bitarray):
+  return bitarray.int()
+  
+def single_boolean(bitarray):
+  return bitarray.int()!=0
+
+def double_int_tuple(bitarray):
+  n = bitarray.size
+  upper=bitarray[:n/2]
+  bottom = bitarray[n/2:n]
+  return upper.int(), bottom.int()
+
+
 if __name__ == '__main__':
   import doctest
   doctest.testfile('base.test', )
