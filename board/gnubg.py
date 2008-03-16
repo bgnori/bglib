6ded66112fff0f80255eea665cd8be1008a1b976
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-03-16 05:45:58 +0000

    added more test. issue: slice is not handled properly. first bit is missing.
    git-svn-id: http://svn.tonic-water.com/projects/public/bgutil/trunk@1019 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- board/gnubg.py
+++ board/gnubg.py
@@ -55,66 +55,88 @@ class BitArray:
     else:
       self.endian = '<' # little endian
 
+  def __len__(self):
+    return self.size
+
+  def int(self):
+    ret = 0
+    mask = 1
+    for bit in self:
+      if bit:
+        ret |= mask
+      mask = mask << 1
+    return ret
+
+
+  def _getbyte(self, pos_of_byte):
+    return struct.unpack(self.strcut_fmt, self.binary[pos_of_byte])[0]
+  
+  def _setbyte(self, pos_of_byte, value):
+    self.binary = (self.binary[:pos_of_byte]
+                   + struct.pack(self.strcut_fmt, value) 
+                   + self.binary[pos_of_byte+1:]
+                   )[:self.size]
+
+  def _pos_in_byte(self, nth):
+    assert(isinstance(nth, int))
+    assert(nth < bits_in_byte)
+    assert(0 <= nth)
+    if self.endian == '<':
+      return nth%bits_in_byte
+    elif self.endian == '>':
+      return 7 - nth%bits_in_byte
+    
   def _getpos(self, nth):
     assert(isinstance(nth, int))
     if 0 >  nth or nth >= self.size:
       raise IndexError('out of range')
+    return (nth/bits_in_byte, self._pos_in_byte(nth%bits_in_byte))
 
-    pos_of_byte = nth/bits_in_byte
 
-    if self.endian == '<':
-      pos_in_byte = nth%bits_in_byte
-    elif self.endian == '>':
-      pos_in_byte =(7 - nth%bits_in_byte) 
+  def getnth(self, nth):
+    pos_of_byte, pos_in_byte = self._getpos(nth)
 
-    return (pos_of_byte, pos_in_byte)
-
-  def __len__(self):
-    return self.size
+    byte = self._getbyte(pos_of_byte)
+    if byte & 1 << pos_in_byte:
+      return 1
+    else:
+      return 0
 
   def __getitem__(self, nth_or_slice):
     if isinstance(nth_or_slice, int):
       return self.getnth(nth_or_slice)
     elif isinstance(nth_or_slice, slice):
-      #assert(nth_or_slice.step == 1)
-      ret = BitArray(size=nth_or_slice.stop - nth_or_slice.start,
-                     endian = self.endian
+      assert(nth_or_slice.step is None)# or nth_or_slice.step == 1)
+      slice_length = nth_or_slice.stop - nth_or_slice.start
+      ret = BitArray(size=slice_length, 
+                     endian=self.endian
                       )
-      for i in range(nth_or_slice.stop -  nth_or_slice.start):
+      for i in range(ret.size):
         ret[i] = self.getnth(nth_or_slice.start + i)
       return ret
     else:
       raise TypeError('index must be int or slice, but got %s'%str(type(nth_or_slice)))
-      
-  def getnth(self, nth):
-    pos_of_byte, pos_in_byte = self._getpos(nth)
-    byte = struct.unpack(self.strcut_fmt, self.binary[pos_of_byte])[0]
-    if byte & 1 << pos_in_byte:
-      return 1
-    else:
-      return 0
 
   def __setitem__(self, nth, value):
     assert(isinstance(nth, int))
     if not value in (0, 1):
       raise ValueError('value for asignment must be 0 or 1')
     pos_of_byte, pos_in_byte = self._getpos(nth)
-
-    data = list(struct.unpack(self.strcut_fmt, self.binary[pos_of_byte]))[0]
+    data = self._getbyte(pos_of_byte)
 
     if value:
-        data |= 1 << pos_in_byte
+      data |= 1 << pos_in_byte
     else:
       data &= ~(1 << pos_of_byte)
-    self.binary = (self.binary[:pos_of_byte]
-                   + struct.pack(self.strcut_fmt, data) 
-                   + self.binary[pos_of_byte+1:]
-                   )[:self.size]
+
+    self._setbyte(pos_of_byte, data)
 
   def __iter__(self):
-    for x in range(self.size):
-      yield self[x]
-      
+    for i in range(self.size):
+      yield self[i]
+
+  def __repr__(self):
+    return "<BitArray Instance '%s'>"%(':'.join(map(str, list(self))))
 
   '''unsupported'''
   def __contains__(self, item):raise NotImplemented
@@ -127,31 +149,46 @@ class BitArray:
   def __imul__(self, x): raise NotImplemented
 
 
+def single_int(bitarray):
+  return bitarray.int()
+  
+def single_boolean(bitarray):
+  return bitarray.int()!=0
+
+def double_int_tuple(bitarray):
+  n = bitarray.size
+  upper=bitarray[:n/2]
+  bottom = bitarray[n/2:n]
+  return upper.int(), bottom.int()
 
 class MatchProxy(object):
   '''you:him'''
   index = dict(
-      cube_in_logarithm=(0, 4, None),
-      cube_owner = (4, 6, None),
-      on_action = (6, 7, None),
-      crawford = (7, 8, None),
-      game_state = (8, 11, None),
-      on_inner_action = (11, 12, None),
-      doubled = (12, 13, None),
-      resign_offer = (13, 15, None),
-      rolled = (15, 21, None),
-      match_length = (21, 36, None),
-      score = (36, 66, None),
+      cube_in_logarithm=(0, 4, single_int),
+      cube_owner = (4, 6, single_int),
+      on_action = (6, 7, single_int),
+      crawford = (7, 8, single_boolean),
+      game_state = (8, 11, single_int),
+      on_inner_action = (11, 12, single_int),
+      doubled = (12, 13, single_boolean),
+      resign_offer = (13, 15, single_int),
+      rolled = (15, 21, double_int_tuple),
+      match_length = (21, 36, single_int),
+      score = (36, 66, double_int_tuple),
       )
 
   def __getattr__(self, name):
     begin, end, func = self.index[name]
-    return self._data[begin]
+    if func:
+      return func(self._data[begin:end])
+    return self._data[begin:end]
 
   def __setattr__(self, name, value):
     pass
+
   def __init__(self, s=None):
-    self.__dict__['_data']=BitArray(66, binary=s)
+    self.__dict__['_data']=BitArray(66, binary=s, endian='<')
+    # assuming s is little endian
 
   def decode(self, s):
     self.__dict__['_data']=BitArray(s)
