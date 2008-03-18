2bc7d10a1eff6b3b10f1ec8cbe63a1e1f8932398
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-03-18 14:00:46 +0000

    impl new style image generator
    git-svn-id: http://svn.tonic-water.com/projects/public/bglib/trunk@1088 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/image.py
+++ src/image.py
@@ -4,89 +4,88 @@
 #
 # Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
 #
+import logging
+import csv
+
 import Image
 import ImageDraw
-import logging
 
 import config
 import model
 
 debug_color = config.active.image.debug_color
-rpath = config.active.image.resource
 
-def draw_point(im, i, O, X):
-    fn = rpath + ''
-    xoffset = 0
-    yoffset = 0
-    rotate = False
-
-    if i > 11:
-      if i < 18:
-        xoffset = 18 * (i-12) + 25
-      else:
-        xoffset = 18 * (i-12) + 50
-      yoffset = 18
+def parse_align():
+  f = file('./resource/align.txt', 'r')
+  for line in f.readlines():
+    x = line.split()
+    if x:
+      yield line.split()
     else:
-      rotate = True
-      if i < 6:
-        xoffset = 18 * (11 - i) + 50
-      else:
-        xoffset = 18 * (11 - i) + 25
-      yoffset = 88 + 38
+      raise StopIteration
+    
+
+class Painter(object):
+  def __init__(self, board):
+    self.image = Image.new("RGB",(291, 232), debug_color)
+    self.board = board
+    self.rpath = config.active.image.resource
+    self.colormap = {model.him:"green-", model.you:"white-"}
+    
+  def point(self, param, x, y):
+    you, him = self.board.position
+    i = int(param) - 1
+    fn = self.rpath
 
     if i % 2:
       fn += "even-"
     else:
       fn += "odd-"
 
-    if O[i] == 0 and X[23-i] ==0:
-      fn += "none.jpg"
-    elif X[23-i]:
-      logging.debug("X x %i @ %i"%(X[23-i],i))
-      fn += "green-"+ str(X[23-i]) + ".jpg"
-    elif O[i]:
-      logging.debug("O x %i @ %i"%(O[i],i))
-      fn += "white-" + str(O[i]) + ".jpg"
+    if you[i]:
+      fn += self.colormap[model.you]+ str(you[i]) + ".jpg"
+    elif him[23-i]:
+      fn += self.colormap[model.him] + str(him[23-i]) + ".jpg"
     else:
-      raise Exception
+      fn += "none.jpg"
 
     pt = Image.open(fn)
-    if rotate:
+    if i < 12:
       pt = pt.rotate(180)
-    print i, xoffset, yoffset
-    im.paste(pt, (xoffset, yoffset))
+    self.image.paste(pt, (x, y))
 
+  def bar(self, param, x, y):
+    you, him = self.board.position
+    if param == 'You':
+      if you[24]:
+        res = Image.open(self.rpath+"bar-"+self.colormap[model.you]+"%i.jpg"%(you[24]))
+      else:
+        res = Image.open(self.rpath+"bar-none.jpg")
+    if param == 'Him':
+      if him[24]:
+        res = Image.open(self.rpath+"bar-"+self.colormap[model.him]+"%i.jpg"%(him[24]))
+      else:
+        res = Image.open(self.rpath+"bar-none.jpg")
+      
+    self.image.paste(res , (x, y))
 
-def draw_bar(im, O, X ):
-  if O[24]:
-    logging.debug("%i"%(O[24]))
-    res = Image.open(rpath+"bar-white-%i.jpg"%(O[24]))
-    logging.debug("%s"%(str(res)))
-  else:
-    res = Image.open(rpath+"bar-none.jpg")
-  im.paste(res , (133, 126))
-  print "O[24]", 133, 126
+  def field(self, param, x, y):
+    self.image.paste(Image.open(self.rpath+"field.jpg"), (x, y))
 
-  if X[24]:
-    res = Image.open(rpath+"bar-green-%i.jpg"%(X[24]))
-  else:
-    res = Image.open(rpath+"bar-none.jpg")
-  im.paste(res, (133, 18))
-  print "X[24]", 133, 18
+  def center(self, param, x, y):
+    self.image.paste(Image.open(self.rpath+"center.jpg"), (133, 106))
+
+  def handle(self, name, param, x, y):
+    f = getattr(self, name)
+    f(param, int(x), int(y))
 
 
 def generate(board):
-  X, O = board.position
-  im = Image.new("RGB",(291, 232), debug_color)
-  for i in range(0,24):
-    draw_point(im, i, O, X)
-  draw_bar(im, O, X)
+  p = Painter(board)
+  for name, param, x, y in parse_align():
+    p.handle(name, param, int(x), int(y))
+  return p.image
 
-  im.paste(Image.open(rpath+"center.jpg"), (133, 106))
-  im.paste(Image.open(rpath+"field.jpg"), (25, 106))
-  im.paste(Image.open(rpath+"field.jpg"), (158, 106))
-
-  return im
 
 if __name__ == '__main__':
   import doctest
