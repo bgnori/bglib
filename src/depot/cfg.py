55908d1c4e600e91d31016c414ed69c12c10af3f
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-04-14 13:48:15 +0000

    impl .cfg proxy
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1225 df26d603-9e29-fa41-8dc3-d72b6cca1104

---
+++ src/depot/cfg.py
@@ -0,0 +1,46 @@
+#!/usrbin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+import ConfigParser
+
+import bglib.depot.base
+
+
+class Proxy(bglib.depot.base.Proxy):
+  def __repr__(self):
+    return "<cfg.Proxy for %s of  %s>"%(self._apth, str(self._impl))
+
+  def _is_in_(self, x):
+    #print 'cfg.Proxy._is_in_', self._apth, x
+    if self._apth:
+      return self._impl.has_option(self._apth[0], x)
+    else:
+      return x in self._impl.sections()
+
+  def _is_node_(self, x):
+    return not bool(self._apth)
+
+  def _get_by_x_(self, x):
+    return self._impl.get(self._apth[0], x)
+
+  def _set_by_x_(self, x, value):
+    self._impl.set(self._apth[0], x, value)
+
+def CFGProxy(filenames):
+  config = ConfigParser.SafeConfigParser()
+  config.read(filenames)
+  return  Proxy(Proxy, config, [])
+
+if __name__ == '__main__':
+  proxy = CFGProxy('fibs.cfg')
+  print proxy.a
+  x = proxy.a
+  print x
+  print x.name
+  print proxy.a.port
+  proxy.a.port = '100'
+  print proxy.a.port
+
