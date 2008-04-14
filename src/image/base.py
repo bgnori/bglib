c464461de14e72c29c70db8c4a6ab9bb321296f3
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-04-14 09:13:53 +0000

    impl NullContext
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1222 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/image/base.py
+++ src/image/base.py
@@ -5,19 +5,17 @@
 # Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
 #
 import logging
-import model
-
 
 class ContextFactroy(object):
   def __init__(self):
     self._cls = dict()
   def register(self, context_class):
-    self._context_classes.update({context_class.name:context_class})
+    self._cls.update({context_class.name:context_class})
   def new_context(self, name, style):
     # ContextFactroy needs size of picture, which is in style
     # Rendering process DOES NOT depend on style
     # thus Renderer object does not have style.
-    return self._context_classes[name](style)
+    return self._cls[name](style)
 
 context_factory = ContextFactroy()
 
@@ -34,7 +32,7 @@ class Style(object):
   def __setattr__(self, name, value):
     self._d[name] = value
 
-class Contenxt(object):
+class Context(object):
   name = 'base'
   def __init__(self, style):
     self._style = style
@@ -43,119 +41,63 @@ class Contenxt(object):
     return self._style
   def result(self):
     return self._result
-  def draw_your_point_at(self, point, checker_count):
-    raise NotImplemented('')
-  def draw_his_point_at(self, point, checker_count):
-    raise NotImplemented('')
-  def draw_empty_point_at(self, point):
-    raise NotImplemented('')
-  def draw_your_bar(self, checker_count):
-    raise NotImplemented('')
-  def draw_his_bar(self, checker_count):
-    raise NotImplemented('')
-  def draw_center_bar(self):
-    raise NotImplemented('')
-  def draw_your_home(self, checker_count);
-    raise NotImplemented('')
-  def draw_his_home(self. checker_count):
-    raise NotImplemented('')
 
-  def draw_cube(self):
-    raise NotImplemented('')
-  def draw_home(self):
-    raise NotImplemented('')
-  def draw_field(self):
-    raise NotImplemented('')
-  def draw_who_to_play(self):
-    raise NotImplemented('')
-  def draw_dice(self):
-    raise NotImplemented('')
-  def draw_center(self):
-    raise NotImplemented('')
+  # points + home +  bar
+  def draw_your_point_at(self, point, checker_count):raise NotImplemented('')
+  def draw_his_point_at(self, point, checker_count):raise NotImplemented('')
+  def draw_empty_point_at(self, point):raise NotImplemented('')
+  def draw_your_bar(self, checker_count):raise NotImplemented('')
+  def draw_his_bar(self, checker_count):raise NotImplemented('')
+  def draw_center_bar(self):raise NotImplemented('')
+  def draw_your_home(self, checker_count):raise NotImplemented('')
+  def draw_his_home(self, checker_count):raise NotImplemented('')
 
-import bglib.image.PIL
-_factory.register(bglib.image.PIL.Context)
+  # cube holder
+  def draw_your_cube(self, cube_value):raise NotImplemented('')
+  def draw_his_cube(self, cube_value):raise NotImplemented('')
+  def draw_center_cube(self, cube_value):raise NotImplemented('')
 
-class Renderer(object):
-  def context(self):
-    return self._context()
+  # field
+  def draw_you_offered_double(self, cube_value):raise NotImplemented('')
+  def draw_he_offered_double(self, cube_value):raise NotImplemented('')
+  def draw_your_dice_in_field(self, dice):raise NotImplemented('')
+  def draw_his_dice_in_field(self, dice):raise NotImplemented('')
 
-  def draw_points(self, board):
-    context = self.context()
-    you, him = board.position
-    for i in range(1, 25):
-      if you[i-1]:
-        context.draw_your_point_at(i, you[i-1])
-      elif him[24-i]:
-        context.draw_his_point_at(i, him[24-i])
-      else:
-        context.draw_empty_point_at(i)
+  # who is on action
+  def draw_you_to_play(self):raise NotImplemented('')
+  def draw_him_to_play(self):raise NotImplemented('')
 
-  def draw_bar(self, board):
-    context = self.context()
-    you, him = board.position
-    context.draw_your_bar(you[24])
-    context.draw_his_bar(him[24])
-    context.draw_center_bar()
+  def draw_frame(self):raise NotImplemented('')
 
-  def draw_home(self, board):
-    context = self.context()
-    you, him = board.position
-    contect.draw_your_home(15 - reduce(lambda x, y: x+y, you))
-    contect.draw_his_home(15 - reduce(lambda x, y: x+y, him))
+class NullContext(Context):
+  name = 'Null'
 
-  def draw_field(self. board):
-    '''Following Cases with 
-      - board.on_action
-      - board.on_inner_action
-    '''
-    context = self.context()
+  # points + home +  bar
+  def draw_your_point_at(self, point, checker_count):pass
+  def draw_his_point_at(self, point, checker_count):pass
+  def draw_empty_point_at(self, point):pass
+  def draw_your_bar(self, checker_count):pass
+  def draw_his_bar(self, checker_count):pass
+  def draw_center_bar(self):pass
+  def draw_your_home(self, checker_count):pass
+  def draw_his_home(self, checker_count):pass
 
-    """ you are on roll cube action. """
-    """ you doubled. he is on take or pass. """
-    """ you double, he took. you are on roll. """
-    """ you rolled x, y. """
-    if board.on_action == model.you and not board.rolled:
-      if not board.doubled and board.on_inner_action == model.you:
-        context.draw_empty_field()
-      if board.doubled and board.on_inner_action == model.him:
-        context.draw_you_offered_double(board.cube_value)
-        context.draw_your_cube(0)
-        context.draw_his_cube(0)
-      if board.doubled and board.on_inner_action == model.you:
-        context.draw_empty_field()
-        context.draw_your_cube(0)
-        context.draw_his_cube(board.cube_value)
+  # cube holder
+  def draw_your_cube(self, cube_value):pass
+  def draw_his_cube(self, cube_value):pass
+  def draw_center_cube(self, cube_value):pass
 
-    if board.on_action == model.you and  board.rolled:
-      context.draw_your_dice_in_field(board.rolled())
+  # field
+  def draw_you_offered_double(self, cube_value):pass
+  def draw_he_offered_double(self, cube_value):pass
+  def draw_your_dice_in_field(self, dice):pass
+  def draw_his_dice_in_field(self, dice):pass
 
-    """ he is  on roll cube action. """
-    """ he doubled. you are on take or pass. """
-    """ he doubled. you took. he is on roll. """
-    """ he rolled x, y. """
-    if board.on_action == model.him and not board.rolled:
-      if not board.doubled and board.on_inner_action == model.him:
-        context.draw_center()
-        pass
-      if board.doubled and board.on_inner_action == model.you:
-        pass
-      if board.doubled and board.on_inner_action == model.him:
-        pass
-    if board.on_action == model.him and  board.rolled:
-      context.draw_his_dice(board.rolled())
+  # who is on action
+  def draw_you_to_play(self):pass
+  def draw_him_to_play(self):pass
 
-  def self.draw_frame(self):
-    context = self.context()
-    context.draw_frame()
+  def draw_frame(self):pass
 
-  def render(context, board):
-    self._context = context
-
-    self.draw_points(board)
-    self.draw_bar(board)
-    self.draw_home(board)
-    self.draw_field(board)
-    self.draw_frame()
-    return context.result()
+context_factory.register(NullContext)
 
---
+++ src/image/renderer.py
@@ -0,0 +1,170 @@
+#!/usrbin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+import logging
+
+import bglib.model
+import bglib.image.base
+import bglib.image.PIL
+
+bglib.image.base.context_factory.register(bglib.image.PIL.Context)
+
+class Renderer(object):
+  def context(self):
+    return self._context
+
+  def draw_points(self, board):
+    context = self.context()
+    you, him = board.position
+    for i in range(1, 25):
+      if you[i-1]:
+        context.draw_your_point_at(i, you[i-1])
+      elif him[24-i]:
+        context.draw_his_point_at(i, him[24-i])
+      else:
+        context.draw_empty_point_at(i)
+
+  def draw_bar(self, board):
+    context = self.context()
+    you, him = board.position
+    context.draw_your_bar(you[24])
+    context.draw_his_bar(him[24])
+    context.draw_center_bar()
+
+  def draw_home(self, board):
+    context = self.context()
+    you, him = board.position
+    context.draw_your_home(15 - reduce(lambda x, y: x+y, you))
+    context.draw_his_home(15 - reduce(lambda x, y: x+y, him))
+
+  def draw_you_are_on_roll_cube_action(self, board):
+    context = self.context()
+    context.draw_you_to_play()
+    context.draw_he_offered_double(0)
+    context.draw_you_offered_double(0)
+    context.draw_your_cube(board.cube_owner == bglib.model.you and board.cube_value or 0)
+    context.draw_his_cube(board.cube_owner == bglib.model.him and board.cube_value or 0)
+    context.draw_center_cube(board.cube_owner == bglib.model.center and board.cube_value or 0)
+
+  def draw_you_doubled_he_is_on_take_or_pass(self, board):
+    context = self.context()
+    context.draw_him_to_play()
+    context.draw_you_offered_double(board.cube_value)
+    context.draw_he_offered_double(0)
+    context.draw_your_cube(0)
+    context.draw_his_cube(0)
+    context.draw_center_cube(0)
+
+  def draw_you_doubled_he_took_you_are_on_roll(self, board):
+    context = self.context()
+    context.draw_you_to_play()
+    context.draw_you_offered_double(0)
+    context.draw_he_offered_double(0)
+    context.draw_your_cube(0)
+    context.draw_his_cube(board.cube_value)
+    context.draw_center_cube(0)
+
+  def draw_you_rolled(self, board):
+    context = self.context()
+    context.draw_you_to_play()
+    context.draw_your_dice_in_field(board.rolled)
+    context.draw_his_dice_in_field(0)
+    context.draw_your_cube(board.cube_owner == bglib.model.you and board.cube_value or 0)
+    context.draw_his_cube(board.cube_owner == bglib.model.him and board.cube_value or 0)
+    context.draw_center_cube(board.cube_owner == bglib.model.center and board.cube_value or 0)
+
+  def draw_he_is_on_roll_cube_action(self, board):
+    context = self.context()
+    context.draw_him_to_play()
+    context.draw_he_offered_double(0)
+    context.draw_you_offered_double(0)
+    context.draw_your_cube(board.cube_owner == bglib.model.you and board.cube_value or 0)
+    context.draw_his_cube(board.cube_owner == bglib.model.him and board.cube_value or 0)
+    context.draw_center_cube(board.cube_owner == bglib.model.center and board.cube_value or 0)
+
+
+  def draw_he_doubled_you_are_on_take_or_pass(self, board):
+    context = self.context()
+    context.draw_you_to_play()
+    context.draw_you_offered_double(0)
+    context.draw_he_offered_double(board.cube_value)
+    context.draw_your_cube(0)
+    context.draw_his_cube(0)
+    context.draw_center_cube(0)
+
+  def draw_he_doubled_you_took_he_is_on_roll(self, board):
+    context = self.context()
+    context.draw_him_to_play()
+    context.draw_you_offered_double(0)
+    context.draw_he_offered_double(0)
+    context.draw_your_cube(board.cube_value)
+    context.draw_his_cube(0)
+    context.draw_center_cube(0)
+
+  def draw_he_rolled(self, board):
+    context = self.context()
+    context.draw_him_to_play()
+    context.draw_your_dice_in_field()
+    context.draw_his_dice_in_field(board.rolled)
+    context.draw_your_cube(board.cube_owner == bglib.model.you and board.cube_value or 0)
+    context.draw_his_cube(board.cube_owner == bglib.model.him and board.cube_value or 0)
+    context.draw_center_cube(board.cube_owner == bglib.model.center and board.cube_value or 0)
+
+  def draw_field(self, board):
+    '''Following Cases with 
+      - board.on_action
+      - board.on_inner_action
+    '''
+    context = self.context()
+
+    if board.on_action == bglib.model.you and not board.rolled:
+      if not board.doubled and board.on_inner_action == bglib.model.you:
+        self.draw_you_are_on_roll_cube_action(board)
+
+      if board.doubled and board.on_inner_action == bglib.model.him:
+        self.draw_you_doubled_he_is_on_take_or_pass(board)
+
+      if board.doubled and board.on_inner_action == bglib.model.you:
+        self.draw_you_doubled_he_took_you_are_on_roll(board)
+
+    if board.on_action == bglib.model.you and  board.rolled:
+      self.draw_you_rolled(board)
+
+    if board.on_action == bglib.model.him and not board.rolled:
+      if not board.doubled and board.on_inner_action == bglib.model.him:
+        self.draw_he_is_on_roll_cube_action(board)
+
+      if board.doubled and board.on_inner_action == bglib.model.you:
+        self.draw_he_doubled_you_are_on_take_or_pass(board)
+
+      if board.doubled and board.on_inner_action == bglib.model.him:
+        self.draw_he_doubled_you_took_he_is_on_roll(board)
+
+    if board.on_action == bglib.model.him and  board.rolled:
+      self.draw_he_rolled(board)
+
+  def draw_frame(self):
+    context = self.context()
+    context.draw_frame()
+
+  def render(self, context, board):
+    self._context = context
+
+    self.draw_points(board)
+    self.draw_bar(board)
+    self.draw_home(board)
+    self.draw_field(board)
+    self.draw_frame()
+    return context.result()
+
+renderer = Renderer()
+if __name__ == '__main__':
+  style = ''
+  context = bglib.image.base.context_factory.new_context('Null', style)
+  board = bglib.model.board()
+  image = renderer.render(context, board)
+
+
