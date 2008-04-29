53f3e383a5fb1d0d56bf9e815419cf91ed3542cd
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-04-29 08:05:36 +0000

    fix added pubsubproxy
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1277 df26d603-9e29-fa41-8dc3-d72b6cca1104

---
+++ src/pubsubproxy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+
+class Proxy(object):
+  def __init__(self, model):
+    self.__dict__['_notify'] = list()
+    self.__dict__['_model'] = model
+  def register(self, notify):
+    self._notify.append(notify)
+  def unregister(self, notify):
+    self._notify.remove(notify)
+  def set_model(self, model):
+    self.__dict__['_model'] = model
+  def __getattr__(self, name):
+    return getattr(self._model, name)
+  def __setattr__(self, name, value):
+    print value
+    setattr(self._model, name, value)
+    for notify in self._notify:
+      notify()
+
