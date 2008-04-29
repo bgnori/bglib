28325dc1e9c1d887c5be16cddfd14d4edb3e8213
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-04-29 11:11:36 +0000

    fix using list for CookieDough
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1280 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/protocol/fibs.py
+++ src/protocol/fibs.py
@@ -38,15 +38,25 @@ class AlphaBatch(Batch):
   FIBS_PlayerMoves = ["^[a-zA-Z_<>]+ moves"]
   FIBS_BearingOff = ["^Bearing off:"]
   FIBS_YouReject = ["^You reject\\. The game continues\\."]
-  FIBS_YouStopWatching = ["You're not watching anymore\\." # overloaded  //PLAYER logs out.. You're not watching anymore.
-  FIBS_OpponentLogsOut = ["The game was saved\\." # PLAYER logs out. The game was saved. ||  PLAYER drops connection. The game was saved.
+  FIBS_YouStopWatching = ["You're not watching anymore\\."]
+  # overloaded  //PLAYER logs out.. You're not watching anymore.
+
+  FIBS_OpponentLogsOut = ["The game was saved\\."]
+  # PLAYER logs out. The game was saved. ||  PLAYER drops connection. The game was saved.
+
   FIBS_OnlyPossibleMove = ["^The only possible move is"]
   FIBS_FirstRoll = ["[a-zA-Z_<>]+ rolled [1-6].+rolled [1-6]"]
   FIBS_MakesFirstMove = [" makes the first move\\."]
-  FIBS_YouDouble = ["^You double\\. Please wait for " # You double. Please wait for PLAYER to accept or reject.
+  FIBS_YouDouble = ["^You double\\. Please wait for "]
+  # You double. Please wait for PLAYER to accept or reject.
+
   FIBS_PlayerWantsToResign = ["^[a-zA-Z_<>]+ wants to resign\\. You will win [0-9]+ points?\\. Type 'accept' or 'reject'\\."]
-  FIBS_WatchResign = ["^[a-zA-Z_<>]+ wants to resign\\. " # PLAYER wants to resign. PLAYER2 will win 2 points.  (ORDER MATTERS HERE)
-  FIBS_YouResign = ["^You want to resign\\." # You want to resign. PLAYER will win 1 .
+  FIBS_WatchResign = ["^[a-zA-Z_<>]+ wants to resign\\. "]
+  # PLAYER wants to resign. PLAYER2 will win 2 points.  (ORDER MATTERS HERE)
+
+  FIBS_YouResign = ["^You want to resign\\."]
+  # You want to resign. PLAYER will win 1 .
+
   FIBS_ResumeMatchAck5 = ["^You are now playing with [a-zA-Z_<>]+\\. Your running match was loaded\\."]
   FIBS_JoinNextGame = ["^Type 'join' if you want to play the next game, type 'leave' if you don't\\."]
   FIBS_NewMatchRequest = ["^[a-zA-Z_<>]+ wants to play a [0-9]+ point match with you\\."]
@@ -55,46 +65,71 @@ class AlphaBatch(Batch):
   FIBS_MatchLength = ["^match length:"]
   FIBS_TypeJoin = ["^Type 'join [a-zA-Z_<>]+' to accept\\."]
   FIBS_YouAreWatching = ["^You're now watching "]
-  FIBS_YouStopWatching = ["^You stop watching " # overloaded
+  FIBS_YouStopWatching = ["^You stop watching "]
+  # overloaded
+
   FIBS_PlayerStartsWatching = ["[a-zA-Z_<>]+ starts watching [a-zA-Z_<>]+\\."]
   FIBS_PlayerStartsWatching = ["[a-zA-Z_<>]+ is watching you\\."]
   FIBS_PlayerStopsWatching = ["[a-zA-Z_<>]+ stops watching [a-zA-Z_<>]+\\."]
   FIBS_PlayerIsWatching = ["[a-zA-Z_<>]+ is watching "]
-  FIBS_ResignWins = ["^[a-zA-Z_<>]+ gives up\\. [a-zA-Z_<>]+ wins [0-9]+ points?\\." # PLAYER1 gives up. PLAYER2 wins 1 point.
+  FIBS_ResignWins = ["^[a-zA-Z_<>]+ gives up\\. [a-zA-Z_<>]+ wins [0-9]+ points?\\."]
+  # PLAYER1 gives up. PLAYER2 wins 1 point.
+
   FIBS_ResignYouWin = ["^[a-zA-Z_<>]+ gives up\\. You win [0-9]+ points?\\."]
   FIBS_YouAcceptAndWin = ["^You accept and win"]
-  FIBS_AcceptWins = ["^[a-zA-Z_<>]+ accepts and wins [0-9]+ point" # PLAYER accepts and wins N points.
-  FIBS_PlayersStartingMatch = ["^[a-zA-Z_<>]+ and [a-zA-Z_<>]+ start a [0-9]+ point match" # PLAYER and PLAYER start a <n> point match.
+  FIBS_AcceptWins = ["^[a-zA-Z_<>]+ accepts and wins [0-9]+ point"]
+  # PLAYER accepts and wins N points.
+
+  FIBS_PlayersStartingMatch = ["^[a-zA-Z_<>]+ and [a-zA-Z_<>]+ start a [0-9]+ point match"]
+  # PLAYER and PLAYER start a <n> point match.
+
   FIBS_StartingNewGame = ["^Starting a new game with "]
   FIBS_YouGiveUp = ["^You give up\\. "]
   FIBS_YouWinMatch = ["^You win the [0-9]+ point match"]
-  FIBS_PlayerWinsMatch = ["^[a-zA-Z_<>]+ wins the [0-9]+ point match" #PLAYER wins the 3 point match 3-0 .
+  FIBS_PlayerWinsMatch = ["^[a-zA-Z_<>]+ wins the [0-9]+ point match"]
+  #PLAYER wins the 3 point match 3-0 .
+
   FIBS_ResumingUnlimitedMatch = ["^[a-zA-Z_<>]+ and [a-zA-Z_<>]+ are resuming their unlimited match\\."]
   FIBS_ResumingLimitedMatch = ["^[a-zA-Z_<>]+ and [a-zA-Z_<>]+ are resuming their [0-9]+-point match\\."]
-  FIBS_MatchResult = ["^[a-zA-Z_<>]+ wins a [0-9]+ point match against " #PLAYER wins a 9 point match against PLAYER  11-6 .
-  FIBS_PlayerWantsToResign = ["wants to resign\\." #  Same as a longline in an actual game  This is just for watching.
+  FIBS_MatchResult = ["^[a-zA-Z_<>]+ wins a [0-9]+ point match against "]
+  #PLAYER wins a 9 point match against PLAYER  11-6 .
+
+  FIBS_PlayerWantsToResign = ["wants to resign\\."]
+  #  Same as a longline in an actual game  This is just for watching.
 
   FIBS_BAD_AcceptDouble = ["^[a-zA-Z_<>]+ accepts? the double\\. The cube shows [0-9]+\\..+"]
   FIBS_YouAcceptDouble = ["^You accept the double\\. The cube shows"]
   FIBS_PlayerAcceptsDouble = ["^[a-zA-Z_<>]+ accepts the double\\. The cube shows "]
-  FIBS_PlayerAcceptsDouble = ["^[a-zA-Z_<>]+ accepts the double\\." # while watching
+  FIBS_PlayerAcceptsDouble = ["^[a-zA-Z_<>]+ accepts the double\\."]
+  # while watching
+
   FIBS_ResumeMatchRequest = ["^[a-zA-Z_<>]+ wants to resume a saved match with you\\."]
   FIBS_ResumeMatchAck0 = ["has joined you\\. Your running match was loaded"]
   FIBS_YouWinGame = ["^You win the game and get"]
   FIBS_UnlimitedInvite = ["^[a-zA-Z_<>]+ wants to play an unlimted match with you\\."]
   FIBS_PlayerWinsGame = ["^[a-zA-Z_<>]+ wins the game and gets [0-9]+ points?. Sorry."]
-  FIBS_PlayerWinsGame = ["^[a-zA-Z_<>]+ wins the game and gets [0-9]+ points?." # (when watching)
+  FIBS_PlayerWinsGame = ["^[a-zA-Z_<>]+ wins the game and gets [0-9]+ points?."]
+  # (when watching)
+
   FIBS_WatchGameWins = ["wins the game and gets"]
-  FIBS_PlayersStartingUnlimitedMatch = ["start an unlimited match\\." # PLAYER_A and PLAYER_B start an unlimited match.
-  FIBS_ReportLimitedMatch = ["^[a-zA-Z_<>]+ +- +[a-zA-Z_<>]+ .+ point match" # PLAYER_A        -       PLAYER_B (5 point match 2-2)
+  FIBS_PlayersStartingUnlimitedMatch = ["start an unlimited match\\."]
+  # PLAYER_A and PLAYER_B start an unlimited match.
+
+  FIBS_ReportLimitedMatch = ["^[a-zA-Z_<>]+ +- +[a-zA-Z_<>]+ .+ point match"]
+  # PLAYER_A        -       PLAYER_B (5 point match 2-2)
+
   FIBS_ReportUnlimitedMatch = ["^[a-zA-Z_<>]+ +- +[a-zA-Z_<>]+ \\(unlimited"]
   FIBS_ShowMovesStart = ["^[a-zA-Z_<>]+ is X - [a-zA-Z_<>]+ is O"]
-  FIBS_ShowMovesRoll = ["^[XO]: \\([1-6]" # ORDER MATTERS HERE
+  FIBS_ShowMovesRoll = ["^[XO]: \\([1-6]"]
+  # ORDER MATTERS HERE
+
   FIBS_ShowMovesWins = ["^[XO]: wins"]
   FIBS_ShowMovesDoubles = ["^[XO]: doubles"]
   FIBS_ShowMovesAccepts = ["^[XO]: accepts"]
   FIBS_ShowMovesRejects = ["^[XO]: rejects"]
-  FIBS_ShowMovesOther = ["^[XO]:" # AND HERE
+  FIBS_ShowMovesOther = ["^[XO]:"]
+  # AND HERE
+
   FIBS_ScoreUpdate = ["^score in [0-9]+ point match:"]
   FIBS_MatchStart = ["^Score is [0-9]+-[0-9]+ in a [0-9]+ point match\\."]
   FIBS_Settings = ["^Settings of variables:"]
@@ -105,7 +140,9 @@ class AlphaBatch(Batch):
   FIBS_Redoubles = ["^redoubles:"]
   FIBS_Sortwho = ["^sortwho:"]
   FIBS_Timezone = ["^timezone:"]
-  FIBS_CantMove = ["^[a-zA-Z_<>]+ can't move" # PLAYER can't move || You can't move
+  FIBS_CantMove = ["^[a-zA-Z_<>]+ can't move"]
+  # PLAYER can't move || You can't move
+
   FIBS_ListOfGames = ["^List of games:"]
   FIBS_PlayerInfoStart = ["^Information about"]
   FIBS_EmailAddress = ["^  Email address:"]
@@ -162,30 +199,48 @@ class AlphaBatch(Batch):
   FIBS_MessagesForYou = ["^There are messages for you:"]
   FIBS_RedoublesSetTo = ["^Value of 'redoubles' set to [0-9]+\\."]
   FIBS_DoublingCubeNow = ["^The number on the doubling cube is now [0-9]+"]
-  FIBS_FailedLogin = ["^> [0-9]+" # bogus CLIP messages sent after a failed login
+  FIBS_FailedLogin = ["^> [0-9]+"]
+  # bogus CLIP messages sent after a failed login
+  
   FIBS_Average = ["^Time (UTC)  average min max"]
   FIBS_DiceTest = ["^[nST]: "]
   FIBS_LastLogout = ["^  Last logout:"]
   FIBS_RatingCalcStart = ["^rating calculation:"]
   FIBS_RatingCalcInfo = ["^Probability that underdog wins:"]
-  FIBS_RatingCalcInfo = ["is 1-Pu if underdog wins" # P=0.505861 is 1-Pu if underdog wins and Pu if favorite wins
-  FIBS_RatingCalcInfo = ["^Experience: " # Experience: fergy 500 - jfk 5832
-  FIBS_RatingCalcInfo = ["^K=max\\(1" # K=max(1 ,    -Experience/100+5) for fergy: 1.000000
+  FIBS_RatingCalcInfo = ["is 1-Pu if underdog wins"]
+  # P=0.505861 is 1-Pu if underdog wins and Pu if favorite wins
+
+  FIBS_RatingCalcInfo = ["^Experience: "]
+  # Experience: fergy 500 - jfk 5832
+
+  FIBS_RatingCalcInfo = ["^K=max\\(1"]
+  # K=max(1 ,    -Experience/100+5) for fergy: 1.000000
+
   FIBS_RatingCalcInfo = ["^rating difference"]
-  FIBS_RatingCalcInfo = ["^change for" # change for fergy: 4*K*sqrt(N)*P=2.023443
+  FIBS_RatingCalcInfo = ["^change for"]
+  # change for fergy: 4*K*sqrt(N)*P=2.023443
+
   FIBS_RatingCalcInfo = ["^match length  "]
   FIBS_WatchingHeader = ["^Watching players:"]
   FIBS_SettingsHeader = ["^The current settings are:"]
   FIBS_AwayListHeader = ["^The following users are away:"]
-  FIBS_RatingExperience = ["^  Rating: +[0-9]+\\." # Rating: 1693.11 Experience: 5781
+  FIBS_RatingExperience = ["^  Rating: +[0-9]+\\."]
+  # Rating: 1693.11 Experience: 5781
+
   FIBS_NotLoggedIn = ["^  Not logged in right now\\."]
   FIBS_IsPlayingWith = ["is playing with"]
-  FIBS_SavedScoreHeader = ["^opponent +matchlength" #  opponent          matchlength   score (your points first)
-  FIBS_StillLoggedIn = ["^  Still logged in\\." #  Still logged in. 2:12 minutes idle.
+  FIBS_SavedScoreHeader = ["^opponent +matchlength"]
+  #  opponent          matchlength   score (your points first)
+
+  FIBS_StillLoggedIn = ["^  Still logged in\\."]
+  #  Still logged in. 2:12 minutes idle.
+
   FIBS_NoOneIsAway = ["^None of the users is away\\."]
   FIBS_PlayerListHeader = ["^No  S  username        rating  exp login    idle  from"]
   FIBS_RatingsHeader = ["^ rank name            rating    Experience"]
-  FIBS_ClearScreen = ["^.\\[;H.\\[2J" # ANSI clear screen sequence
+  FIBS_ClearScreen = ["^.\\[;H.\\[2J"]
+  # ANSI clear screen sequence
+
   FIBS_Timeout = ["^Connection timed out\\."]
   FIBS_Goodbye = ["           Goodbye\\."]
   FIBS_LastLogin = ["^  Last login:"]
@@ -196,10 +251,16 @@ class NumericBatch(Batch):
   CLIP_WHO_INFO = ["^5 [^ ]+ [^ ]+ - [01]"]
   CLIP_WHO_INFO = ["^5 [^ ]+ - [^ ]+ [01]"]
 
-  FIBS_Average = ["^[0-9][0-9]:[0-9][0-9]-" # output of average command
-  FIBS_DiceTest = ["^[1-6]-1 [0-9]" # output of dicetest command
+  FIBS_Average = ["^[0-9][0-9]:[0-9][0-9]-"]
+  # output of average command
+
+  FIBS_DiceTest = ["^[1-6]-1 [0-9]"]
+  # output of dicetest command
+
   FIBS_DiceTest = ["^[1-6]: [0-9]"]
-  FIBS_Stat = ["^[0-9]+ bytes" # output from stat command
+  FIBS_Stat = ["^[0-9]+ bytes"]
+  # output from stat command
+
   FIBS_Stat = ["^[0-9]+ accounts"]
   FIBS_Stat = ["^[0-9]+ ratings saved. reset log"]
   FIBS_Stat = ["^[0-9]+ registered users."]
@@ -223,18 +284,26 @@ class NumericBatch(Batch):
 
 class StarsBatch(Batch):
   FIBS_Username = ["^\\*\\* User"]
-  FIBS_Junk = ["^\\*\\* You tell " # "** You tell PLAYER: xxxxx"]
+  FIBS_Junk = ["^\\*\\* You tell "]
+  # "** You tell PLAYER: xxxxx"]
+
   FIBS_YouGag = ["^\\*\\* You gag"]
   FIBS_YouUngag = ["^\\*\\* You ungag"]
   FIBS_YouBlind = ["^\\*\\* You blind"]
   FIBS_YouUnblind = ["^\\*\\* You unblind"]
   FIBS_UseToggleReady = ["^\\*\\* Use 'toggle ready' first"]
   FIBS_NewMatchAck9 = ["^\\*\\* You are now playing an unlimited match with "]
-  FIBS_NewMatchAck10 = ["^\\*\\* You are now playing a [0-9]+ point match with " # ** You are now playing a 5 point match with PLAYER
-  FIBS_NewMatchAck2 = ["^\\*\\* Player [a-zA-Z_<>]+ has joined you for a" # ** Player PLAYER has joined you for a 2 point match.
+  FIBS_NewMatchAck10 = ["^\\*\\* You are now playing a [0-9]+ point match with "]
+  # ** You are now playing a 5 point match with PLAYER
+
+  FIBS_NewMatchAck2 = ["^\\*\\* Player [a-zA-Z_<>]+ has joined you for a"]
+  # ** Player PLAYER has joined you for a 2 point match.
+
   FIBS_YouTerminated = ["^\\*\\* You terminated the game"]
   FIBS_OpponentLeftGame = ["^\\*\\* Player [a-zA-Z_<>]+ has left the game. The game was saved\\."]
-  FIBS_PlayerLeftGame = ["has left the game\\." # overloaded
+  FIBS_PlayerLeftGame = ["has left the game\\."]
+  # overloaded
+
   FIBS_YouInvited = ["^\\*\\* You invited"]
   FIBS_YourLastLogin = ["^\\*\\* Last login:"]
   FIBS_NoOne = ["^\\*\\* There is no one called"]
@@ -280,13 +349,17 @@ class StarsBatch(Batch):
   FIBS_AlreadyPlaying = ["is already playing with"]
   FIBS_DidntInvite = ["^\\*\\* [a-zA-Z_<>]+ didn't invite you."]
   FIBS_BadMove = ["^\\*\\* You can't remove this piece"]
-  FIBS_CantMoveFirstMove = ["^\\*\\* You can't move " # ** You can't move 3 points in your first move
+  FIBS_CantMoveFirstMove = ["^\\*\\* You can't move "]
+  # ** You can't move 3 points in your first move
+
   FIBS_CantShout = ["^\\*\\* Please type 'toggle silent' again before you shout\\."]
   FIBS_MustMove = ["^\\*\\* You must give [1-4] moves"]
   FIBS_MustComeIn = ["^\\*\\* You have to remove pieces from the bar in your first move\\."]
   FIBS_UsersHeardYou = ["^\\*\\* [0-9]+ users? heard you\\."]
   FIBS_Junk = ["^\\*\\* Please wait for [a-zA-Z_<>]+ to join too\\."]
-  FIBS_SavedMatchReady = ["^\\*\\*[a-zA-Z_<>]+ +[0-9]+ +[0-9]+ +- +[0-9]+" # double star before a name indicates you have a saved game with this player
+  FIBS_SavedMatchReady = ["^\\*\\*[a-zA-Z_<>]+ +[0-9]+ +[0-9]+ +- +[0-9]+"]
+  # double star before a name indicates you have a saved game with this player
+
   FIBS_NotYourTurnToRoll = ["^\\*\\* It's not your turn to roll the dice\\."]
   FIBS_NotYourTurnToMove = ["^\\*\\* It's not your turn to move\\."]
   FIBS_YouStopWatching = ["^\\*\\* You stop watching"]
@@ -298,7 +371,9 @@ class StarsBatch(Batch):
   FIBS_PlayerNotPlaying = ["^\\*\\* [a-zA-Z_<>]+ is not playing\\."]
   FIBS_CantTalk = ["^\\*\\* You can't talk if you won't listen\\."]
   FIBS_WontListen = ["^\\*\\* [a-zA-Z_<>]+ won't listen to you\\."]
-  FIBS_Why = ["Why would you want to do that" # (not sure about ** vs *** at front of line.)
+  FIBS_Why = ["Why would you want to do that"]
+  # (not sure about ** vs *** at front of line.)
+
   FIBS_Ratings = ["^\\* *[0-9]+ +[a-zA-Z_<>]+ +[0-9]+\\.[0-9]+ +[0-9]+"]
   FIBS_NoSavedMatch = ["^\\*\\* There's no saved match with "]
   FIBS_WARNINGSavedMatch = ["^\\*\\* WARNING: Don't accept if you want to continue"]
@@ -310,7 +385,8 @@ class LoginBatch(Batch):
   CLIP_WELCOME = ["^1 [a-zA-Z_<>]+ [0-9]+ "]
   CLIP_OWN_INFO = ["^2 [a-zA-Z_<>]+ [01] [01]"]
   CLIP_MOTD_BEGIN = ["^3$"]
-  FIBS_FailedLogin = ["^> [0-9]+" # bogus CLIP messages sent after a failed login
+  FIBS_FailedLogin = ["^> [0-9]+"]
+  # bogus CLIP messages sent after a failed login
 
 class MOTDBatch(Batch):
   CLIP_MOTD_END = ["^4$"]
