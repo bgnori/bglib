deb6a18d0e8e552acb5e2c14569bf6739153f687
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-05-04 17:57:47 +0000

    fix pubsubproxy notifies on set_model
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1307 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/pubsubproxy.py
+++ src/pubsubproxy.py
@@ -7,18 +7,21 @@
 
 class Proxy(object):
   def __init__(self, model):
-    self.__dict__['_notify'] = list()
+    self.__dict__['_subscriber'] = list()
     self.__dict__['_model'] = model
   def register(self, notify):
-    self._notify.append(notify)
+    self._subscriber.append(notify)
   def unregister(self, notify):
-    self._notify.remove(notify)
+    self._subscriber.remove(notify)
   def set_model(self, model):
     self.__dict__['_model'] = model
+    self._notify()
   def __getattr__(self, name):
     return getattr(self._model, name)
   def __setattr__(self, name, value):
     setattr(self._model, name, value)
-    for notify in self._notify:
+    self._notify()
+  def _notify(self):
+    for notify in self._subscriber:
       notify()
 
