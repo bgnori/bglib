67c948c12f66d27280c69402647e1d9c410a9f3d
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-05-14 04:34:46 +0000

    fix file path for test case
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1357 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/depot/cfg.py
+++ src/depot/cfg.py
@@ -53,7 +53,7 @@ def CFGProxy(filenames):
   return  Proxy(Proxy, config, [])
 
 if __name__ == '__main__':
-  proxy = CFGProxy('connection.cfg')
+  proxy = CFGProxy(['./connection.cfg'])
   print proxy.a
   x = proxy.a
   print x
