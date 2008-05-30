4feb6b2840374ae14b95b69039e7f13a0e1a3aee
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-05-30 05:38:37 +0000

    added testcase
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1476 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/gui/testframe.py
+++ src/gui/testframe.py
@@ -107,6 +107,9 @@ class InteractiveTester(wx.Frame):
            ('22wqECCw8+ABYA', 'UQmgAAAAAAAA'),
            ('4HPiASHgc/ABMA', 'UQn1AAAAAAAA'),
            ('4HPKATDgc/ABMA', 'cAngAAAAAAAA'),
+
+           ('4Dl4ADqwt4MDIA', 'MBmgAAAAAAAA'),
+
            ('PwkAACoBAAAAAA', 'cAn2AAAAAAAA'),
            ('FwAA4CcBAAAAAA', 'MAH2AAAAAAAA'),
            ('4HPiASHgc/ABMA', 'UQn1AAAAAAAA'),
