dcb85f130f52b7f4fd8e723c2ef3729968cbe16b
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-06-17 02:10:16 +0000

    added file, to make css.py to produce css specification
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1566 df26d603-9e29-fa41-8dc3-d72b6cca1104

---
+++ src/image/testcss.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+import bglib.image.css
+import doctest
+doctest.testfile('css.test', )
