167af2de955975deee3daf4572d6eb0ec8a4e061
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-07-08 08:18:01 +0000

    fix one item per one import
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1654 df26d603-9e29-fa41-8dc3-d72b6cca1104

---
+++ src/protocol/session_test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+
+import subprocess
+import signal
+import os
+import time
+
+import unittest
+import nose
+
+from bglib.protocol.session import Session
+from bglib.protocol.session import Subscriber
+from bglib.protocol.session import Transport
+from bglib.protocol.session import synchronized_with
+
+class SessionTest(unittest.TestCase):
+  def setUp(self):
+    self.serverps = subprocess.Popen(('python',
+                                     './bglib/protocol/server.py',
+                                     './bglib/protocol/fibs_test.in',),
+                                     )
+                                     #stdin=None, stdout=None, stderr=None,)
+    time.sleep(.5)
+    self.session = Session()
+    assert not self.session.is_active()
+
+  def tearDown(self):
+    if self.session.is_active():
+      self.session.close()
+    assert not self.session.is_active()
+    os.kill(self.serverps.pid, signal.SIGKILL)
+    self.serverps.wait()
+
+  def subscriber_1_test(self):
+    sub = Subscriber()
+    self.session.register([sub])
+    self.session.open('localhost', 4321)
+    self.session.unregister([sub])
+
+  def subscriber_2_test(self):
+    sub = Subscriber()
+    self.session.open('localhost', 4321)
+    self.session.register([sub])
+    self.session.unregister([sub])
+
+  def subscriber_3_test(self):
+    class PreLogin(Subscriber):
+      flag = False
+      def CLIP_MOTD_END(self, got):pass
+      def CLIP_OWN_INFO(self, got):pass
+      def CLIP_WELCOME(self, got):pass
+      def CLIP_MOTD_BEGIN(self, got):pass
+      def FIBS_MOTD(self, got):
+        print 'FIBS_MOTD'
+      @synchronized_with(Transport.lock)
+      def FIBS_PreLogin(self, got):
+        print 'FIBS_PreLogin'
+        self.flag = True
+    sub = PreLogin()
+    self.assertFalse(sub.flag)
+    self.session.register([sub])
+    self.session.open('localhost', 4321)
+    time.sleep(2)
+    self.assert_(sub.flag)
+    self.session.unregister([sub])
+
+
