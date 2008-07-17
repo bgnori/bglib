3008f3f4b8397944fd929d017c00a6f65063f80e
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-07-17 09:59:58 +0000

    fix added data path for testing
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1666 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- src/image/resource/minimal.py
+++ src/image/resource/minimal.py
@@ -4,17 +4,22 @@
 #
 # Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
 #
+import os.path
 import bglib.image.PIL
 import bglib.image.css
-css = bglib.image.css.load("./bglib/image/resource/minimal/default.css")
+from bglib.image.theme import themata
+
+mypath = 'minimal'
+testdatapath = os.path.join(themata, mypath, 'testdata')
+
+css = bglib.image.css.load(os.path.join(themata, mypath, "default.css"))
 draw = bglib.image.PIL.Draw(css)
 
 if __name__ == '__main__':
   import bglib.image.writer
   w = bglib.image.writer.Writer()
-  f = file('minimal_test.py', 'w')
+  f = file(os.path.join(themata, mypath, 'test.py'), 'w')
   w.write(f, 'minimal')
   f.close()
-
 
 
