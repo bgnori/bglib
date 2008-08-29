5d4f023e57e817dde2d1b27bf8bb5297c4255e97
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-08-29 06:50:08 +0000

    impl Node, Editor, Visitor
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1779 df26d603-9e29-fa41-8dc3-d72b6cca1104

---
+++ src/doc/doctree.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+
+class Node(object):
+  def __init__(self, parent):
+    assert not parent or isinstance(parent, Node)
+    self.parent = parent
+    self.children = list()
+
+  def on_enter(self, visitor):
+    visitor.enter(self)
+
+  def on_leave(self, visitor):
+    visitor.leave(self)
+
+  def append(self, node):
+    assert isinstance(node, Node)
+    self.children.append(node)
+
+  def remove(self, node):
+    assert isinstance(node, Node)
+    assert node in self.children
+    self.children.remove(node)
+
+  def accept(self, visitor):
+    assert isinstance(visitor, Visitor)
+    self.on_enter(visitor)
+    for c in self.children:
+      c.accept(visitor)
+    self.on_leave(visitor)
+
+  def __str__(self):
+    return "%s"%(self.__class__.__name__)
+  def __repr__(self):
+    return "%s:%x"%(self.__class__.__name__, id(self))
+
+
+class Root(Node):
+  def __init__(self):
+    Node.__init__(self, None)
+  def __repr__(self):
+    return "Root:%x"%(id(self))
+  def __str__(self):
+    return "Root"
+
+
+class BgWikiElementNode(Node):
+  html_element = Node
+  def __init__(self, parent, **d):
+    Node.__init__(self, parent)
+    self.attrs = dict(**d)
+
+  def acceptables(self):
+    return ()
+
+  def is_acceptable(self, e):
+    return isinstance(e, self.acceptables())
+
+  def open(self):
+    if self.attrs:
+      return '<%s'%self.html_element + \
+             ' '.join(['%s=%s'%(key, item) for key, item in self.attrs.items()]) + \
+             '>'
+    else:
+      return '<%s>'%self.html_element
+
+  def close(self):
+    return '</%s>'%self.html_element
+
+
+class LineElement(BgWikiElementNode):
+  def acceptables(self):
+    return (SpanElement,)
+
+
+class SpanElement(LineElement):
+  pass
+
+class BoldElement(SpanElement):
+  html_element = 'strong'
+
+class ItalicElement(SpanElement):
+  html_element = 'i'
+
+class UnderlineElement(SpanElement):
+  html_element = 'span'
+  def open(self):
+    return '<span class="underline">'
+
+class StrikeElement(SpanElement):
+  html_element = 'del'
+
+class SubscriptElement(SpanElement):
+  html_element = 'sub'
+
+class SuperscriptElement(SpanElement):
+  html_element = 'sup'
+
+class MonospaceElement(SpanElement):
+  html_element = 'span'
+  def open(self):
+    return '<span class="monospace">'
+
+
+class TableCellElement(LineElement):
+  html_element = 'td'
+
+class TableRowElement(LineElement):
+  html_element = 'tr'
+  def acceptables(self):
+    return super(TableRowElement, self).acceptables() + \
+          (TableCellElement,)
+
+
+class ItemizeElement(LineElement):
+  html_element = 'li'
+  def acceptables(self):
+    return super(ItemizeElement, self).acceptables() + \
+          (ListElement, ItemizeElement)
+    
+  def open(self):
+    style = self.attrs['style']
+    if style is None:
+      return '<%s>'%(self.html_element)
+    else:
+      m = self.attrs.get('minus', None) or self.attrs.get('plus', None)
+      return '<%s %s>'%(self.html_element, style[m])
+
+
+class DefinitionHeaderElement(LineElement):
+  html_element = 'dt'
+
+class DefinitionBodyElement(LineElement):
+  html_element = 'dd'
+
+class AnchorElement(LineElement):
+  html_element = 'a'
+  def set_url(self, url):
+    self.url = url
+  def open(self):
+    return '<a href="%s">'%self.url
+
+class BoxElement(BgWikiElementNode):
+  def acceptables(self):
+    return (SpanElement,)
+  def open(self):
+    return '<%s>\n'%self.html_element
+  def close(self):
+    return '</%s>\n'%self.html_element
+  
+
+class DefinitionListElement(BoxElement):
+  html_element = 'dl'
+  def acceptables(self):
+    return super(DefinitionListElement, self).acceptables() + \
+           (DefinitionHeaderElement, DefinitionBodyElement)
+
+
+class TableElement(BoxElement):
+  html_element = 'table'
+  def acceptables(self):
+    return super(TableElement, self).acceptables() + \
+          (TableRowElement, TableCellElement)
+  def open(self):
+    return '<table class="wiki">\n' # \n for readability
+
+
+class BlockQuoteElement(BoxElement):
+  def open(self):
+    return '<blockquote>\n<p>\n'
+  def close(self):
+    return '</p>\n</blockquote>\n'
+
+
+class CitationContentElement(LineElement):
+  html_element = 'p'
+  def acceptables(self):
+    return super(CitationContentElement, self).acceptables() + \
+           (CitationElement, CitationContentElement)
+  def open(self):
+    return '<p>\n'
+  def close(self):
+    return '</p>\n'
+
+
+
+class CitationElement(BoxElement):
+  def acceptables(self):
+    return super(CitationElement, self).acceptables() + \
+         (CitationElement, CitationContentElement)
+  def open(self):
+    return '<blockquote class="citation">\n'
+  def close(self):
+    return '</blockquote>\n'
+
+class ListElement(BoxElement):
+  _design = dict(star=('ul', None, None),
+              sign=('ul', 'class="sign"', 
+               {'+':'class="plus"', # class names for li elements
+                '++':'class="doubleplus"', 
+                '+++':'class="tripleplus"',
+                '-':'class="minus"', 
+                '--':'class="doubleminus"', 
+                '---':'class="tripleminus"'}),
+             ordered_numeric=('ol', None, None),
+             ordered_alpha=('ol', 'class="loweralpha"', None),
+             ordered_roman=('ol', 'class="lowerroman"', None),
+             )
+  def acceptables(self):
+    return super(ListElement, self).acceptables() + \
+           (ListElement, ItemizeElement)
+
+  def get_design_name(self):
+    for key in self._design:
+      if self.attrs[key]:
+        return key
+    return 'star' #default design name
+
+  def get_tag_info(self):
+    return self._design[self.get_design_name()][:2]
+
+  def get_style(self):
+    return self._design[self.get_design_name()][2]
+
+  def open(self):
+    self.count = 0
+    tag, attrs = self.get_tag_info()
+    if attrs:
+      return '<%s %s>\n'%(tag, attrs)
+    else:
+      return '<%s>\n'%(tag)
+
+  def close(self):
+    tag, attrs = self.get_tag_info()
+    return '</%s>\n'%(tag)
+
+
+
+class Text(BgWikiElementNode):
+  def __init__(self, parent, text=None, **d):
+    BgWikiElementNode.__init__(self, parent, **d)
+    if not text:
+      text = ''
+    self.set_text(text)
+
+  def set_text(self, text):
+    self.text = text
+
+  def append_text(self, text):
+    self.text += text
+
+  def __repr__(self):
+    return "Text:%x %s"%(id(self), self.text)
+
+  def __str__(self):
+    return "Text:%s"%(self.text)
+
+  def open(self):
+    return self.text
+
+  def close(self):
+    return ''
+
+
+class Editor(object):
+  def __init__(self):
+    self.root = None
+    self.current = None
+
+  def start(self, root):
+    assert isinstance(root, Root)
+    self.root = root
+    self.current = root
+
+  def done(self):
+    self.current = self.root
+    return self.root
+
+  def enter(self, klass, **d):
+    self.current = self.append(klass, **d)
+
+  def leave(self, *klasses):
+    node = self.current
+    while node:
+      if isinstance(node, *klasses):
+        self.current = node.parent
+        return
+      node = node.parent
+      #implicitly leaving node.
+    assert False #ugh! No such node!
+
+  def append(self, klass, **d):
+    assert issubclass(klass, Node)
+    node = klass(self.current, **d)
+    self.current.append(node)
+    return node
+
+  def remove(self, node):
+    self.current.remove(node)
+
+  def append_text(self, text):
+    if self.current.children:
+      n = self.current.children[-1]
+      if isinstance(n, Text):
+        n.append_text(text)
+        return
+    self.current.append(Text(self.current, text))
+
+  def ancestor(self, *klassses):
+    node = self.current
+    while node:
+      if isinstance(node, *klassses):
+        return node
+      node = node.parent
+    return None
+
+
+
+class Visitor(object):
+  def enter(self, node):
+    pass
+  def leave(self, node):
+    pass
+
+
+class DebugVisitor(Visitor):
+  def __init__(self):
+    self.buf = ''
+  def enter(self, node):
+    self.buf += str(node)+ '\n'
+
+
+class PrintVisitor(Visitor):
+  def enter(self, node):
+    print node
+
+
+class HtmlVisitor(Visitor):
+  def enter(self, node):
+    print node.open()
+  def leave(self, node):
+    print node.close()
+
+
---
+++ src/doc/doctree_test.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim: syntax=python
+#
+# Copyright 2006-2008 Noriyuki Hosaka nori@backgammon.gr.jp
+#
+import unittest
+import bglib.doc.doctree
+
+class NodeTest(unittest.TestCase):
+  def setUp(self):
+    self.root = bglib.doc.doctree.Root()
+
+  def test_is_acceptable_of_Span(self):
+    b = bglib.doc.doctree.BoldElement(self.root)
+    i = bglib.doc.doctree.ItalicElement(self.root)
+    isinstance(b, bglib.doc.doctree.SpanElement)
+    isinstance(i, bglib.doc.doctree.SpanElement)
+    self.assert_(i.is_acceptable(b))
+    self.assert_(b.is_acceptable(i))
+
+    self.root.append(b)
+    self.root.append(i)
+    d = bglib.doc.doctree.DebugVisitor()
+    self.root.accept(d)
+
+    self.assertEqual(
+      d.buf,
+      ('Root\n'
+      'BoldElement\n'
+      'ItalicElement\n')
+    )
+    
+  def test_is_acceptable_of_itemize(self):
+    b = bglib.doc.doctree.BoldElement(self.root)
+    i = bglib.doc.doctree.ItemizeElement(self.root)
+    o = bglib.doc.doctree.ListElement(self.root)
+    isinstance(i, bglib.doc.doctree.LineElement)
+    isinstance(o, bglib.doc.doctree.BoxElement)
+    self.assert_(i.is_acceptable(b))
+    self.assert_(o.is_acceptable(i))
+    self.assert_(o.is_acceptable(b))
+
+    self.assertFalse(b.is_acceptable(o))
+    self.assertFalse(b.is_acceptable(i))
+
+
+class EditorTest(unittest.TestCase):
+  def setUp(self):
+    self.root = bglib.doc.doctree.Root()
+    self.editor = bglib.doc.doctree.Editor()
+
+  def test_start_done(self):
+    editor = self.editor
+    editor.start(self.root)
+    t = editor.done()
+    self.assertEqual(t, self.root)
+
+  def test_enter(self):
+    editor = self.editor
+    editor.start(self.root)
+    editor.enter(bglib.doc.doctree.Node)
+    self.assertNotEqual(self.root, self.editor.current)
+    t = editor.done()
+    self.assertEqual(t, self.root)
+  
+  def test_leave(self):
+    editor = self.editor
+    editor.start(self.root)
+    editor.enter(bglib.doc.doctree.BgWikiElementNode)
+    editor.enter(bglib.doc.doctree.Node)
+    editor.leave(bglib.doc.doctree.BgWikiElementNode)
+    self.assertEqual(editor.current, self.root)
+    t = editor.done()
+    self.assertEqual(t, self.root)
+
+  def test_append_text(self):
+    editor = self.editor
+    editor.start(self.root)
+    editor.append_text('ho')
+    editor.append_text('ge')
+    editor.enter(bglib.doc.doctree.BgWikiElementNode)
+    self.assertEqual(len(editor.current.children), 0)
+    editor.append_text('pi')
+    self.assertEqual(len(editor.current.children), 1)
+    editor.append_text('yo')
+    self.assertEqual(len(editor.current.children), 1)
+    editor.leave(bglib.doc.doctree.BgWikiElementNode)
+    editor.append_text('foo')
+    editor.append_text('bar')
+    t = editor.done()
+    d = bglib.doc.doctree.DebugVisitor()
+    self.root.accept(d)
+    self.assertEqual(
+      d.buf,
+      ('Root\n'
+      'Text:hoge\n'
+      'BgWikiElementNode\n'
+      'Text:piyo\n'
+      'Text:foobar\n'
+      )
+    )
+    self.assertEqual(len(t.children), 3)# Text, BgWikiElementNode, Text
+
+class BgWikiElementNodeTest(unittest.TestCase):
+  def setUp(self):
+    pass
+
+
