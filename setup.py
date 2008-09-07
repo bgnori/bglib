06ac653077b6875cd371790a2aaa79131bcf77a6
nori <nori@df26d603-9e29-fa41-8dc3-d72b6cca1104>
2008-09-07 04:25:32 +0000

    fix removed obsolete MANIFEST
    git-svn-id: http://svn.tonic-water.com/projects/proprietary/bglib/trunk@1811 df26d603-9e29-fa41-8dc3-d72b6cca1104

--- setup.py
+++ setup.py
@@ -9,30 +9,65 @@ from distutils.core import setup
 import os.path
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 setup(
-  name='backgammonbase-bgutil',
+  name='python-bglib-library',
   version=__version__,
   #zip_safe=False,
-  description="backgammon programming utilities",
+  description="backgammon programming utilities for python",
   long_description=
 """This package contains:
-  encoding subpackage
-  - base module is basic component for encoding subpackage
-  - gnubg module is gnubg postion id decoder/decoder
-  - urlsafe module is urlsafe version of gnubg position decoder/encoder
-  - FIBS module is FIBS 'board:' responce  decoder
+  * model 
+   this module provides model to all other modules in this library.
+  * depot
+   this module provides config loader in various formats.
+  * encoding 
+   - base module provides basic operations to other modules in this subpackage
+   - gnubg module is gnubg postion id decoder/decoder
+   - urlsafe module is urlsafe version of gnubg position decoder/encoder
+   - FIBS module is FIBS 'board:' responce  decoder
+  * image 
+   this module provides board imagin functions using PIL.
+  * gui 
+   this module provides some GUI widgets using wxpython.
+  * doc
+   this  module provides bgwiki formatting engine.
 """,
   author="Noriyuki Hosaka",
   author_email="bgnori@gmail.com",
-  package_dir = {'bglib':'src',},
-  packages = ['bglib', 'bglib.encoding', 'bglib.image', 'bglib.gui', 'bglib.protocol'],
-  package_data = {'bglib.image.resource':[os.path.join('image','resource','*.jpg'),], },
-  py_modules=[
-      'bglib.model',
-      ],
+  package_dir = {
+                 'bglib':'src',
+                 },
+#                 'bglib.depot':'src/depot',
+#                 'bglib.doc':'src/doc',
+#                 'bglib.encoding':'src/encoding',
+#                 'bglib.gui':'src/gui',
+#                 'bglib.image':'src/image',
+#                 'bglib.model':'src/model',
+#                 'bglib.protocol':'src/protocol',
+#                 },
+  packages = ['bglib', 
+#              'bglib.depot', 
+#              'bglib.doc', 
+#              'bglib.encoding', 
+#              'bglib.gui', 
+#              'bglib.image', 
+#              'bglib.model',
+#              'bglib.protocol'
+              ],
+  package_data = {
+      'bglib.image.resource.matrix':[os.path.join('image','resource','matrix','default.css'),
+                                     os.path.join('image','resource','matrix','*.png'),
+                                    ],
+      'bglib.image.resource.minimal':[os.path.join('image','resource','matrix','default.css'),
+                                    ],
+      'bglib.image.resource.safari':[os.path.join('image','resource','safari','default.css'),
+                                     os.path.join('image','resource','safari','*.jpg'),
+                                    ],
+      },
+  py_modules=[],
   url="http://www.backgammonbase.com",
   license="proprietary",
 )
