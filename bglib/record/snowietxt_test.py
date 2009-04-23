941d00aa56a706dd8b63b1827bd4924e89bc45b6
Noriyuki Hosaka <nori@asama.tonic-water.com>
2009-04-24 01:16:23 +0900

    fixed testcase 	modified:   bglib/record/snowietxt_test.py

--- bglib/record/snowietxt_test.py
+++ bglib/record/snowietxt_test.py
@@ -12,15 +12,19 @@ from bglib.record.snowietxt import Validator
 class SnowietxtTest(unittest.TestCase):
   def test1857680(self):
     f = open('bglib/record/snowietxt/1857680.txt')
-    v = Validator(f)
-    h = v.validate()
+    v = Validator()
+    for line in f:
+      v.feed(line)
+    h = v.done()
     self.assertEqual(h.hexdigest(),
                      'a1d23325d0c8a614756529de960e4e7bdc1f6737')
   def testbad(self):
     f = StringIO.StringIO('hogehoge\nzooba\n')
-    v = Validator(f)
+    v = Validator()
     try:
-      h = v.validate()
+      for line in f:
+        v.feed(line)
+      h = v.done()
       self.assert_(False)
     except ValueError:
       pass
